06f7ada511f57dfb1241d6761264ff73e2e071b6
linlin.fu <linlin.fu@thoughtworks.com>
2019-06-15 10:23:45 +0800

    [linlin] #1.1 add book and bibliote service and welcome

---
+++ src/com/twu/biblioteca/BibliotecaService.java
@@ -0,0 +1,8 @@
+package com.twu.biblioteca;
+
+public class BibliotecaService {
+
+    public void welcome() {
+        System.out.println("Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore");
+    }
+}
